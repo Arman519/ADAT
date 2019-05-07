1500c6deb25129075efc8551c5c649ff98060374
Arman Ramazyan <arman.ramazyan@gmail.com>
2019-05-06 23:32:04 -0700

    my_changes

--- Backup1/Extras/Crossword Generator/TransparentToolStrip.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class TransparentToolStripRender
-    Inherits ToolStripProfessionalRenderer
-    Protected Overrides Sub OnRenderToolStripBackground(ByVal e As System.Windows.Forms.ToolStripRenderEventArgs)
-        'MyBase.OnRenderToolStripBackground(e)
-    End Sub
-    Protected Overrides Sub OnRenderToolStripBorder(ByVal e As System.Windows.Forms.ToolStripRenderEventArgs)
-        'MyBase.OnRenderToolStripBorder(e)
-    End Sub
-End Class
